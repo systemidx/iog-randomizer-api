8988217e11f3fb8d418d6dc59366ff3fe82b95e8
Bryon Weber <bryon.weber@gmail.com>
2019-09-05 14:04:29 -0400

    Refactor to Python

--- Randomizer.Api/Models/RandomizerRomCreationDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Randomizer.Api.Models
-{
-    public struct RandomizerRomCreationDetails
-    {
-        public bool Success {get; set;}
-        public string RomPath { get; set; }
-        public string Error {get; set;}
-    }
-}
