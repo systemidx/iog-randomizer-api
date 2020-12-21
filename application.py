585f97c023bf428e28b4b81afd7a31b340c3f9b5
Bryon Weber <bryon.weber@gmail.com>
2020-12-20 20:59:50 -0500

    Updates from dev branch

--- application.py
+++ application.py
@@ -31,7 +31,6 @@ cors = CORS(app, resources={
 config = Config()
 database = Database(logging, config)
 
-
 @app.route("/v1/seed/generate", methods=["POST"])
 @expects_json(SeedRequest.schema)
 def generateSeed() -> Response:
@@ -42,7 +41,8 @@ def generateSeed() -> Response:
                         request_data.ohko, request_data.red_jewel_madness, request_data.allow_glitches,
                         request_data.boss_shuffle, request_data.open_mode, request_data.z3_mode, request_data.overworld_shuffle, request_data.entrance_shuffle)
 
-    result = __generate(settings, request_data.generate_race_rom, 0)
+    randomizer = Randomizer("./data/gaia.bin")
+    result = __generate(randomizer, settings, request_data.generate_race_rom, 0)
     if result is None:
         return make_response("Failed to generate a seed", 500)
 
@@ -83,7 +83,7 @@ def getRandomizerVersion() -> Response:
     return make_response(version.to_json())
 
 
-def __generate(settings: Settings, race: bool = False, retries: int = 0) -> Result:
+def __generate(randomizer: Randomizer, settings: Settings, race: bool = False, retries: int = 0) -> Result:
     if retries >= 3:
         return None
 
@@ -96,17 +96,17 @@ def __generate(settings: Settings, race: bool = False, retries: int = 0) -> Resu
         logging.info(f"({settings.seed}) Generating patch ({retries + 1} / 3)...")
         logging.info(f"({settings.seed}) Settings: {str(request.get_json())}")
 
-        patch = __generatePatch(settings)
+        patch = __generatePatch(randomizer, settings)
         if patch is None:
             logging.info(f"({settings.seed}) Failed to generate patch in {time.perf_counter() - patch_start_time} seconds!")
-            return __generate(settings, race, retries + 1)
+            return __generate(randomizer, settings, race, retries + 1)
         else:
             logging.info(f"({settings.seed}) Generated patch in {time.perf_counter() - patch_start_time} seconds!")
 
         if not race:
             logging.info(f"({settings.seed}) Race Mode off, generating spoiler...")
             spoiler_start_time = time.perf_counter()
-            spoiler = __generateSpoiler(settings)
+            spoiler = __generateSpoiler(randomizer, settings)
             logging.info(f"({settings.seed}) Generated spoiler in {time.perf_counter() - spoiler_start_time} seconds!")
         else:
             logging.info(f"({settings.seed}) Race Mode on, not generating spoiler...")
@@ -125,13 +125,13 @@ def __generate(settings: Settings, race: bool = False, retries: int = 0) -> Resu
         return __generate(settings, race, retries + 1)
 
 
-def __generatePatch(settings: Settings) -> Patch:
+def __generatePatch(randomizer: Randomizer, settings: Settings) -> Patch:
     patch_filename = generate_filename(settings, "sfc")
     patch = randomizer.generate_rom(patch_filename, settings)
     return Patch(patch, patch_filename, VERSION)
 
 
-def __generateSpoiler(settings: Settings) -> Spoiler:
+def __generateSpoiler(randomizer: Randomizer, settings: Settings) -> Spoiler:
     spoiler_filename = generate_filename(settings, "txt")
     spoiler = randomizer.generate_spoiler()
     return Spoiler(spoiler, spoiler_filename)
