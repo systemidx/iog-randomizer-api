a10cfb92bbb42c7307bb7ca409aa866260dfa795
Bryon Weber <bryon.weber@gmail.com>
2021-01-14 14:12:27 -0500

    Updates start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Starting IOGR API"
-source /var/lib/iogr/variables.conf && python3 /var/lib/iogr/application.py
+source /var/lib/iogr/variables.conf \
+&& pip3 install virtualenv \
+&& python3 -m virtualenv /var/lib/iogr/env \
+&& sudo /var/lib/iogr/env/bin/pip install -r /var/lib/iogr/requirements.txt \
+&& /var/lib/iogr/env/bin/python /var/lib/iogr/application.py
