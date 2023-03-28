cc3346edb33f916047dc8996420d538e15f5366b
Bryon Weber <bryon.weber@gmail.com>
2023-03-28 12:59:10 -0400

    Fixes pathing

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 echo "Starting IOGR API"
 source variables.conf
 pip install virtualenv
-python -m virtualenv ./app/env
-./app/env/bin/pip install -r ./app/requirements.txt
-./app/env/bin/python ./app/application.py
+python -m virtualenv /home/bryon/app/env
+/home/bryon/app/env/bin/pip install -r /home/bryon/app/requirements.txt
+/home/bryon/app/env/bin/python /home/bryon/app/application.py
